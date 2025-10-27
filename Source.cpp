ccabdb315bca306001b4056e86d59cc7a3d5f06b
manh <nguyentuanmanh11112004@gmail.com>
2025-10-27 11:22:21 +0700

    chuc nang kiem tra so le

--- Source.cpp
+++ Source.cpp
@@ -15,9 +15,19 @@ int main()
     cin >> b;
 	cout<<"Tong= : "<<tong(a,b)<<endl;
 	cout<<"Hieu= : "<<hieu(a,b)<<endl;
+	  cout << "Nhap so can kiem tra so le: ";
+    cin >> n;
+    if (kiemTraSoLe(n)) {
+        cout << n << " la so le." << endl;
+    } else {
+        cout << n << " la so chan." << endl;
+    }
 	system("pause");
 	return 0;
 }
+	bool kiemTraSoLe(int n) {
+    return n % 2 != 0;
+	}
 	int tong(int a, int b){
 		return a+b;
 	}
