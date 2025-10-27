581ecfb121658d1002d52b09a871e6383635856f
manh <nguyentuanmanh11112004@gmail.com>
2025-10-27 11:14:38 +0700

    da sua lai chuc nang tong va hieu

--- Source.cpp
+++ Source.cpp
@@ -7,6 +7,7 @@ int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
+	int a,b;
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	cout << "Nhap so thu nhat: ";
     cin >> a;
@@ -14,13 +15,12 @@ int main()
     cin >> b;
 	cout<<"Tong= : "<<tong(a,b)<<endl;
 	cout<<"Hieu= : "<<hieu(a,b)<<endl;
-	
+	system("pause");
+	return 0;
+}
 	int tong(int a, int b){
 		return a+b;
 	}
 	int hieu(int a, int b){
 		return a-b;
 	}
-	system("pause");
-	return 0;
-}
